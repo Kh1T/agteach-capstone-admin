f71435a9caa4d8211bf36e4e431c93a38eebd058
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-03 20:02:29 +0700

    chore: add ignore rbt config

--- src/pages/NewCategory.jsx
+++ src/pages/NewCategory.jsx
@@ -1,3 +1,37 @@
+import {  Button, Divider, Stack, TextField, Typography } from "@mui/material";
+
 export default function NewCategoryPage() {
-    return <div>Setting</div>;
+  return (
+    <Stack gap={3}>
+      <ContentText
+        title="What is the category name for the product?"
+        text="Your category name should be short and meaningful"
+      />
+      <TextField label="Title" />
+      <ContentText
+        title="Tell us more about your category"
+        text="Help explain what does the category contain"
+      />
+      <TextField
+        id="outlined-multiline-static"
+        multiline
+        label="Description"
+        placeholder="Tell us more about your category"
+        rows={5}
+      />
+      <Divider sx={{borderStyle:'dashed'}}/>
+      <Button variant="contained" size="large" sx={{ width:"200px", backgroundColor: "purple.main" }}>
+        CREATE CATEGORY
+      </Button>
+    </Stack>
+  );
+}
+
+function ContentText({ title, text }) {
+  return (
+    <Stack gap>
+      <Typography variant="blgsm">{title}</Typography>
+      <Typography variant="bxsr">{title}</Typography>
+    </Stack>
+  );
 }
