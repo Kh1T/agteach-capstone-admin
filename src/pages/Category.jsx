c8b51597217cc297a50d7661b925640f45b9d137
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-03 16:07:07 +0700

    feat: add table and header of category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,3 +1,36 @@
+import { Box } from "@mui/material";
+import QueryHeader from "../components/QueryHeader";
+import { useRef, useState } from "react";
+import TableComponent from "../components/TableComponent";
+import { data } from "../data/sampleDashboardData";
 export default function CategoryPage() {
-    return <div>Category</div>;
+  const [selectState, setSelectState] = useState(0);
+  const searchRef = useRef();
+  const label = "Sort";
+
+  /**
+   * Handles search button click event
+   * @function
+   * @name handleSearch
+   * @param {Event} event - The search button click event
+   * @description Logs the search input value and the selected sort option
+   */
+  function handleSearch() {
+    console.log(searchRef.current.value);
+    console.log(selectState);
+  }
+  return (
+    <Box width={"100%"}>
+      <QueryHeader
+        label={label}
+        searchRef={searchRef}
+        useSelectState={[selectState, setSelectState]}
+        selectData={["Newest", "Oldest"]}
+        handleSearch={handleSearch}
+        pathCreated="/category/new"
+        labelCreate="Create Category"
+      />
+      <TableComponent data={data} />
+    </Box>
+  );
 }
