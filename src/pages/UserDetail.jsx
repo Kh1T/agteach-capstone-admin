a8c9a35d20d296662197a7590bcfaeb70549279d
sela-bet <betsela555@gmail.com>
2024-09-04 19:50:02 +0700

    chore: use CustomTable component

--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -13,20 +13,17 @@ import {
   Typography,
 } from '@mui/material';
 import ChevronLeft from '@mui/icons-material/ChevronLeft';
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+import CustomTable from '../components/CustomTable';
 
 export default function UserDetailPage() {
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-
+  const instructorList = instructors.map((item) => ({
+    ...item,
+    password: (
+      <Button color="error" variant="outlined">
+        Reset
+      </Button>
+    ),
+  }));
   return (
     <Stack alignItems="start" gap={5}>
       <Link>
@@ -68,37 +65,22 @@ export default function UserDetailPage() {
       <Divider sx={{ borderStyle: 'dashed', width: '100%' }} />
       <Stack width="100%" gap={2}>
         <Typography variant="blgsm">User Detail</Typography>
-        <TableContainer sx={{ boxShadow: 'none' }}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right">{row.carbs}</TableCell>
-                  <TableCell align="right">{row.protein}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <CustomTable data={instructorList} />
       </Stack>
-      
     </Stack>
   );
 }
+
+const instructors = [
+  {
+    id: 1,
+    firstName: 'Chan',
+    lastName: 'Dara',
+    username: 'chandara',
+    email: 'dara@abc.xyz',
+    phone: '0123456789',
+    address: 'St.6, plov veng sreng',
+    createdAt: '2022-01-01T00:00:00.000Z',
+    lastLogin: '2022-01-01T00:00:00.000Z',
+  },
+];
