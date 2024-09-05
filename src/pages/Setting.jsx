c74a8e3b5319d6be806d49c32936fa7186a259f4
sela-bet <betsela555@gmail.com>
2024-09-05 09:02:23 +0700

    init: initialize setting core component

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,3 +1,19 @@
+import { Stack, Typography, Button } from '@mui/material';
+
 export default function SettingPage() {
-    return <div>Setting</div>;
+  return (
+    <Stack>
+      <Typography variant="h4">Password Modification</Typography>
+      <Typography variant="subtitle1">Current Password</Typography>
+      <input type="password" />
+      <Typography variant="subtitle1">New Password</Typography>
+      <input type="password" />
+      <Typography variant="subtitle1">Confirm New Password</Typography>
+      <input type="password" />
+      <Stack direction="row" spacing={2}>
+        <Button variant="contained">Save Changes</Button>
+        <Button variant="outlined">Cancel</Button>
+      </Stack>
+    </Stack>
+  );
 }
