dccf69d90c7115c3b2bfac886bb3270d0ef78ac0
sovanchitra <chivsovanchetra@gmail.com>
2024-09-20 14:50:21 +0700

    fix: change to cloud api

--- src/store/api/authApi.js
+++ src/store/api/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001", // corrected from baseQuery to baseUrl
+    baseUrl: "https://api.agteach.site", // corrected from baseQuery to baseUrl
     credentials: "include",
   }),
   endpoints: (builder) => ({
