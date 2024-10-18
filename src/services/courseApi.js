f89ebed6ef1f14aa0fedfd7c52962b959244e5ea
Khit Khom <khomkhit460@gmail.com>
2024-10-18 13:56:17 +0700

    fix-admin-review: change base url

--- src/services/courseApi.js
+++ src/services/courseApi.js
@@ -3,8 +3,8 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 export const courseApi = createApi({
   reducerPath: "courseApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://api.agteach.site",
-    baseUrl: "http://localhost:3001",
+    baseUrl: "https://api.agteach.site",
+    // baseUrl: "http://localhost:3001",
     credentials: "include",
   }),
   tagTypes: ["Course"],
