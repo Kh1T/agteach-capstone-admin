4c3ec44f29d9e7e6fc440adc8bf09b7c0258a10f
Pav Munyphalla <lalaphalla@gmail.com>
2024-11-21 11:36:44 +0700

    refactor: update filename theme folder

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,7 +1,7 @@
 import { createTheme } from "@mui/material";
-import color from "./Color";
-import typography from "./Typography";
-import components from "./Component";
+import color from "./colors";
+import typography from "./typographys";
+import components from "./components";
 
 const theme = createTheme({
     palette: color,
